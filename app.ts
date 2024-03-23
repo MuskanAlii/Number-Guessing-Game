7cb053ca7a6fe9db2354f47cc8687b52ee6735a3
Muskan <159627108+MuskanAlii@users.noreply.github.com>
2024-03-23 21:08:24 +0500

    Number guessing game

---
+++ app.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.hex("#FB5607")("Welcome to Number Guessing Game! "));
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const result = await inquirer.prompt([
+  {
+    name: "GuessedNumber",
+    type: "number",
+    message: chalk.hex("#FF8FAB")("Please! Guess the Number Between 1-6: "),
+  },
+]);
+
+if (result.GuessedNumber === randomNumber) {
+  console.log(
+    chalk.hex("#57CC99")(
+      "Congratulations! You Guessed The Right Number (YOU WIN)"
+    )
+  );
+} else {
+  console.log(chalk.hex("#BF0603")("You Guessed Wrong Number (TRY AGAIN) "));
+}
